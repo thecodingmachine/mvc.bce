1e89448cbc9cfa6acdfb06ee5462b5010bf1c840
Ibrahima <iso@bivolis.com>
2012-12-11 18:23:16 +0100

    add span renderer

---
+++ src/Mouf/MVC/BCE/classes/SpanRenderer.php
@@ -0,0 +1,30 @@
+<?php
+namespace Mouf\MVC\BCE\classes;
+
+/**
+ * This renderer handles Read-Only fields
+ * @ApplyTo { "pk" : [ "pk" ] }
+ * @Component
+ */
+class SpanRenderer implements SingleFieldRendererInterface {
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see FieldRendererInterface::render()
+	 */
+	public function render($descriptor){
+		/* @var $descriptor BaseFieldDescriptor */
+		$fieldName = $descriptor->getFieldName();
+		$value = $descriptor->getFieldValue();
+		return "<span>".$value."</span>";
+	}
+	
+	/**
+	 * (non-PHPdoc)
+	 * @see FieldRendererInterface::getJS()
+	 */
+	public function getJS($descriptor){
+		return array();
+	}
+	
+}
