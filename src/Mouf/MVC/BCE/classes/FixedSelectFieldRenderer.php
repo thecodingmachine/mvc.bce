f3d4ce80ecad818a0327e893d9465e94c0b17427
IBRAHIMA <iso@bivolis.com>
2013-01-18 12:01:13 +0100

    Add FixedSelectFieldRenderer

---
+++ src/Mouf/MVC/BCE/classes/FixedSelectFieldRenderer.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Mouf\MVC\BCE\classes;
+
+/**
+ * This renderer handles Read-Only fields
+ * @ApplyTo { "pk" : [ "pk" ] }
+ * @Component
+ */
+class FixedSelectFieldRenderer implements SingleFieldRendererInterface {
+
+    /**
+     * Tells if the field should display a select box or a radio button group
+     * @Property
+     * @var bool
+     */
+    public $radioMode = false;
+
+    /**
+     * (non-PHPdoc)
+     * @see FieldRendererInterface::render()
+     */
+    public function render($descriptor) {
+        /* @var $descriptor ForeignKeyFieldDescriptor */
+        $fieldName = $descriptor->getFieldName();
+        $data = $descriptor->getData();
+        $value = $descriptor->getFieldValue();
+        $html = "";
+
+        $selected = " - ";
+        foreach ($data as $linkedBean) {
+            $beanId = $descriptor->getRelatedBeanId($linkedBean);
+            $beanLabel = $descriptor->getRelatedBeanLabel($linkedBean);
+            if ($beanId == $value)
+                $selected = $beanLabel;
+        }
+
+        $html .= "<span style='padding-top:5px;display:block;'>" . $selected . "</span>";
+        return $html;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see FieldRendererInterface::getJS()
+     */
+    public function getJS($descriptor) {
+        return array();
+    }
+
+}
