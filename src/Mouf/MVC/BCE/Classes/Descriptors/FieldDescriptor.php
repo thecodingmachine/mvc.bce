c30563a39c850c5b58007cf43b1381d903e45dc2
Pierre Vaidie <pierre.vaidie@gmail.com>
2013-11-12 17:07:55 +0100

    enable fine for label

--- src/Mouf/MVC/BCE/Classes/Descriptors/FieldDescriptor.php
+++ src/Mouf/MVC/BCE/Classes/Descriptors/FieldDescriptor.php
@@ -51,7 +51,7 @@ abstract class FieldDescriptor implements BCEFieldDescriptorInterface {
 	/**
 	 * The label of the field as displayed in the form
 	 * @Property
-	 * @var string
+	 * @var string|ValueInterface
 	 */
 	public $label;
 	
@@ -195,7 +195,7 @@ abstract class FieldDescriptor implements BCEFieldDescriptorInterface {
 	 * Returns the label of the field
 	 */
 	public function getFieldLabel(){
-		return $this->label;
+		return ValueUtils::val($this->label);
 	}
 	
 	/**
