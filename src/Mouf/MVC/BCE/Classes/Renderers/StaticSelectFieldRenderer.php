993c46da87132bca45059a89f071a0a5a8a98589
jeremiepose <j.pose@thecodingmachine.com>
2014-02-20 10:55:57 +0100

    replace broken StaticSelectRenderer by a new class

---
+++ src/Mouf/MVC/BCE/Classes/Renderers/StaticSelectFieldRenderer.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Mouf\MVC\BCE\Classes\Renderers;
+
+use Mouf\MVC\BCE\Classes\Descriptors\FieldDescriptorInstance;
+use Mouf\Html\Widgets\Form;
+use Mouf\Html\Widgets\Form\TextField;
+use Mouf\MVC\BCE\Classes\ValidationHandlers\BCEValidationUtils;
+use Mouf\Html\Tags\Span;
+use Mouf\MVC\BCE\Classes\Renderers\DefaultViewFieldRenderer;
+use Mouf\MVC\BCE\Classes\Renderers\SingleFieldRendererInterface;
+use Mouf\MVC\BCE\Classes\BCEException;
+
+/**
+ * This renderer handles select field with static options and values
+ *
+ * Class StaticSelectFieldRenderer
+ * @package Mouf\MVC\BCE
+ */
+class StaticSelectFieldRenderer  extends DefaultViewFieldRenderer implements SingleFieldRendererInterface {
+
+    protected $options;
+
+    /**
+     * @param array<string, string> $options
+     */
+    public function __construct($options) {
+        $this->options = $options;
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see FieldRendererInterface::render()
+     */
+    public function renderEdit($descriptorInstance){
+
+        $selectField = new Form\SelectField($descriptorInstance->fieldDescriptor->getFieldLabel(), $descriptorInstance->getFieldName(), $descriptorInstance->getFieldValue(), $this->options);
+        $selectField->setRequired(BCEValidationUtils::hasRequiredValidator($descriptorInstance->fieldDescriptor->getValidators()));
+
+        ob_start();
+        $selectField->toHtml();
+        return ob_get_clean();
+    }
+
+    /**
+     * (non-PHPdoc)
+     * @see FieldRendererInterface::getJS()
+     */
+    public function getJSEdit($descriptor, $bean, $id){
+        /* @var $descriptorInstance FieldDescriptorInstance */
+        return array();
+    }
+}
